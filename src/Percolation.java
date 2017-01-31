c7babd72f63a562fcb75df5037763b8cb4fdbbdd
Eddie Chou <eddieechou@gmail.com>
2017-01-31 04:05:17 -0800

    Add java files

---
+++ src/Percolation.java
@@ -0,0 +1,150 @@
+/*----------------------------------------------------------------
+ *  Author:        Eddie Chou
+ *  Written:       1/31/2017
+ *  Last updated:  1/31/2017
+ *
+ *  Compilation:   javac Percolation.java
+ *  
+ *  Percolation model class. 
+ *
+ *----------------------------------------------------------------*/
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+	private int gridSize;		// Grid size of the model
+	private int numOpenSites;	// Keeps track of the number of open sites
+	
+	// Possible states that a site can be in
+	private static final byte CLOSED = 0;
+	private static final byte OPEN = 1;
+	private static final byte CONNECTED_TOP = 2;
+	private static final byte CONNECTED_BOTTOM = 4;
+	private static final byte CONNECTED_BOTH = 7;
+	private byte[] state;
+	
+	private WeightedQuickUnionUF connections;	// The union-find data type					
+	private boolean hasPercolated;				// A flag for if the model percolates
+	
+	// create n-by-n grid, with all sites blocked
+   public Percolation(int n){
+	   if(n < 0) throw new IllegalArgumentException();
+	   hasPercolated = false;
+	   gridSize = n;
+	   
+	   int numSites = xyTo1D(n+1, n+1);
+	   numOpenSites = 0;
+	   state = new byte[numSites];	// 1 to n indexing
+	   // Initialize all states to closed (false)
+	   for(int i = 1; i < n+1; i++){
+		   for(int j = 1; j < n+1; j++){
+			   state[xyTo1D(i,j)] = CLOSED;
+		   }
+	   }
+	   connections = new WeightedQuickUnionUF(numSites);	// All sites initially in own component
+   }
+   
+   // open site (row, col) if it is not open already
+   public void open(int row, int col){
+	   checkBounds(row, col);
+	   int site = xyTo1D(row, col);
+	   int top;
+	   int left;
+	   int right;
+	   int bot;
+	   byte topState = 0, leftState = 0, rightState = 0, botState = 0;
+	   if((state[site] & OPEN) == OPEN){
+		   return;
+	   }
+	   state[site] |= OPEN;
+	   
+	   if(row==1){
+		   state[site] |= CONNECTED_TOP;
+	   }
+	   if(row==gridSize){
+		   state[site] |= CONNECTED_BOTTOM;
+	   }
+	   int neighbor;
+	   // Set up WeightedQuickUnionUF unions
+	   // If top is open
+	   if(row > 1 && isOpen(row-1, col)){
+		   top = xyTo1D(row-1, col);
+		   neighbor = connections.find(top);	// gets root of top
+		   topState = state[neighbor];			// gets state of top's root
+		   connections.union(site, neighbor);	// connect top and site
+	   }
+	   // If left is open
+	   if(col > 1 && isOpen(row, col-1)){
+		   left = xyTo1D(row, col-1);
+		   neighbor = connections.find(left);	// gets root of left
+		   leftState = state[neighbor];			// gets state of left's root
+		   connections.union(site, neighbor);
+	   }
+	   // If right is open
+	   if(col < gridSize && isOpen(row, col+1)){
+		   right = xyTo1D(row, col+1);
+		   neighbor = connections.find(right);	// gets root of left
+		   rightState = state[neighbor];			// gets state of left's root
+		   connections.union(site, neighbor);
+	   }
+	   // If bottom is open
+	   if(row < gridSize && isOpen(row+1, col)){
+		   bot = xyTo1D(row+1, col);
+		   neighbor = connections.find(bot);	// gets root of left
+		   botState = state[neighbor];			// gets state of left's root
+		   connections.union(site, neighbor);
+	   }
+	   
+	   int newRoot = connections.find(site);
+	   state[newRoot] |= (byte)(topState | leftState | rightState | botState | state[site]);
+	   // Check if newRoot is connected to both top and bottom
+	   if((state[newRoot] & CONNECTED_BOTH) == CONNECTED_BOTH){
+		   hasPercolated = true;
+	   }
+	   numOpenSites++;
+   }
+   
+   private void checkBounds(int row, int col){
+	   if(row < 1 || row > gridSize || col < 1 || col > gridSize){
+		   throw new IndexOutOfBoundsException();
+	   }
+   }
+   
+   private void checkBoundsState(int row, int col){
+	   if(row < 0 || row > gridSize || col < 0 || col > gridSize){
+		   throw new IndexOutOfBoundsException();
+	   }
+   }
+   
+   // Translate to start = 0 (top-left will be 1, 1)
+   private int xyTo1D(int row, int col){
+	   return (row * gridSize) + col;
+   }
+   
+   // is site (row, col) open?
+   public boolean isOpen(int row, int col){
+	   checkBoundsState(row, col);
+	   return state[xyTo1D(row, col)] > 0;	// return state of root
+   }
+   
+   // is site (row, col) full/closed?
+   public boolean isFull(int row, int col){
+	   checkBounds(row, col);
+	   return (state[connections.find(xyTo1D(row, col))] & CONNECTED_TOP) == CONNECTED_TOP;	// Find state of root 
+   }
+   
+   // number of open sites
+   public int numberOfOpenSites(){
+	   return numOpenSites;
+   }
+   // does the system percolate?
+   // Connect from virtual point at top, and at bottom
+   public boolean percolates(){
+	   return hasPercolated;
+   }
+   
+   // test client (optional)
+   public static void main(String[] args){
+	   
+   }
+}
---
+++ src/PercolationStats.java
@@ -0,0 +1,76 @@
+/*----------------------------------------------------------------
+ *  Author:        Eddie Chou
+ *  Written:       1/31/2017
+ *  Last updated:  1/31/2017
+ *
+ *  Compilation:   javac PercolationStats.java
+ *  Execution:     java PercolationStats n t
+ *  
+ *  Estimate the value of the percolation threshold via Monte Carlo 
+ *  simulation.
+ *  Uses an n x n grid and t trials.
+ *
+ *  $ java PercolationStats 2 10000
+ * mean                    = 0.6668
+ * stddev                  = 0.11780979549592113
+ * 95% confidence interval = [0.6644909280082799, 0.66910907199172]
+ *
+ *----------------------------------------------------------------*/
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+	private int numTrials;	// The number of trials to perform
+	Percolation perc;		// Percolation model object
+	double[] results;		// Results of each trial (ratio of open/closed sites)
+	
+	// perform trials independent experiments on an n-by-n grid
+   public PercolationStats(int n, int trials){
+	   if(n<=0 || trials<=0) throw new java.lang.IllegalArgumentException();
+	   numTrials = trials;
+	   results = new double[trials];
+	   // Run trials
+	   for(int i=0; i<numTrials; i++){
+		   perc = new Percolation(n);
+		   while(!perc.percolates()){
+			   int row = StdRandom.uniform(1, n+1);
+			   int col = StdRandom.uniform(1, n+1);
+			   perc.open(row, col);
+		   }
+		   
+		   results[i] = (double)perc.numberOfOpenSites() / (double)(n*n);
+	   }
+
+   }
+   
+   // sample mean of percolation threshold
+   public double mean(){
+	   return StdStats.mean(results);
+   }
+   
+   // sample standard deviation of percolation threshold
+   public double stddev(){
+	   return StdStats.stddev(results);
+   }
+   
+   // low  endpoint of 95% confidence interval
+   public double confidenceLo(){
+	   return mean() - (1.96*stddev() / Math.sqrt(numTrials));
+   }
+   
+   // high endpoint of 95% confidence interval
+   public double confidenceHi(){
+	   return  mean() + (1.96*stddev()/ Math.sqrt(numTrials));
+   }
+   
+   // test client (described below)
+   public static void main(String[] args) {
+	   int n = Integer.parseInt(args[0]);
+	   int trials = Integer.parseInt(args[1]);
+	   PercolationStats perc = new PercolationStats(n, trials);
+	   System.out.println("mean                    = " + perc.mean());
+	   System.out.println("stddev                  = " + perc.stddev());
+	   System.out.println("95% confidence interval = [" + perc.confidenceLo() + ", " + perc.confidenceHi() + "]");
+   }
+}
